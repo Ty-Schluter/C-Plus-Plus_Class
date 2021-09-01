c72cffc23893358a8ba4c18b0b1c6047435785b7
Ty-S <schluter2@tritons.iowacentral.edu>
2021-09-01 10:52:24 -0500

    Add project files.

---
+++ 1st Lab Of C++/Source.cpp
@@ -0,0 +1,14 @@
+/* Ty Schluter
+* C++ 1st 2021
+* September 3rd
+* Lab 1 Introduction to C++ Programming
+* This lab will serve as a demonstration of your ability to download a project from GitHub, compile and run the project, upload a project to GitHub, and submit a project to Google Classroom.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+		return 0;
+}
